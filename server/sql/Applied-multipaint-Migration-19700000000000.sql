d89e33fe0fee502338a9f986108b9e8a8a19a5c2
Marko Elezovic <marko@element.hr>
2015-02-24 10:13:41 +0100

    a

--- server/sql/Applied-multipaint-Migration-19700000000000.sql
+++ server/sql/Applied-multipaint-Migration-19700000000000.sql
@@ -1,1 +1,5 @@
-CREATE DATABASE multipaint WITH OWNER multipaint ENCODING 'utf8' TEMPLATE template1;
+CREATE DATABASE multipaint WITH
+  OWNER = multipaint
+  ENCODING = 'utf8'
+  LC_COLLATE = 'en_US.UTF-8'
+  LC_CTYPE = 'en_US.UTF-8';
